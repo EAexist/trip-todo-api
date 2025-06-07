57e0fa3dd96da66abc71eae3b02f3012be081200
Pyo Hyeon <megatwins@kaist.ac.kr>
2025-06-08 05:03:37 +0900

    first-commit

---
+++ src/main/java/com/matchalab/trip_todo_api/TripTodoApiApplication.java
@@ -0,0 +1,13 @@
+package com.matchalab.trip_todo_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripTodoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripTodoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matchalab/trip_todo_api/TripTodoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matchalab.trip_todo_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripTodoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
