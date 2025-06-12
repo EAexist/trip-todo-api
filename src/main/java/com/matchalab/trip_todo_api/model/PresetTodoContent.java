e538627e280333d8a9d45b09ea15062a6078b142
Pyo Hyeon <megatwins@kaist.ac.kr>
2025-06-13 05:33:05 +0900

    feat: serve request to patch contents of todo

--- src/main/java/com/matchalab/trip_todo_api/model/PresetTodoContent.java
+++ src/main/java/com/matchalab/trip_todo_api/model/PresetTodoContent.java
@@ -2,32 +2,32 @@ package com.matchalab.trip_todo_api.model;
 
 import java.util.List;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-@RequiredArgsConstructor
+@NoArgsConstructor
 public class PresetTodoContent extends TodoContent {
 
     @Id
     private Long id;
 
-    @OneToMany(mappedBy = "presetTodoContent", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Todo> todo;
+    // @OneToMany(mappedBy = "presetTodoContent", cascade = CascadeType.ALL,
+    // orphanRemoval = true)
+    // private List<Todo> todo;
 
     public PresetTodoContent(List<Todo> todo, Long id,
             String category,
             String type,
             String title,
             String iconId) {
-        super(category, type, title, iconId);
-        this.todo = todo;
+        super(id, category, type, title, iconId);
+        this.id = id;
+        // this.todo = todo;
     }
 }
