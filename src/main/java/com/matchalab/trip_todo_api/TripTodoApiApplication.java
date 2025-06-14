02f4326e7c23b519e8fc19464fc069c948a98fa7
Pyo Hyeon <megatwins@kaist.ac.kr>
2025-06-14 11:00:21 +0900

    feat: configure CORS to allow f/e client

--- src/main/java/com/matchalab/trip_todo_api/TripTodoApiApplication.java
+++ src/main/java/com/matchalab/trip_todo_api/TripTodoApiApplication.java
@@ -1,13 +1,18 @@
 package com.matchalab.trip_todo_api;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class TripTodoApiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TripTodoApiApplication.class, args);
     }
-
 }
