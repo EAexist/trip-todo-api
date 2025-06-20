28b64a75020e24997b47498570a14b71a3d4239c
Pyo Hyeon <megatwins@kaist.ac.kr>
2025-06-20 13:00:41 +0900

    fix: fix test settings

--- src/test/java/com/matchalab/trip_todo_api/PostgresqlDBConfigTest.java
+++ src/test/java/com/matchalab/trip_todo_api/PostgresqlDBConfigTest.java
@@ -25,8 +25,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @TestInstance(Lifecycle.PER_CLASS)
 @SpringJUnitConfig(classes = {}, initializers = ConfigDataApplicationContextInitializer.class)
-@TestPropertySource(properties = { "spring.config.location = classpath:application-test.yml" })
-@ActiveProfiles("test")
+@TestPropertySource(properties = { "spring.config.location =classpath:application-dev.yml" })
+@ActiveProfiles("dev")
 public class PostgresqlDBConfigTest {
 
     @Value("${spring.datasource.url}")
